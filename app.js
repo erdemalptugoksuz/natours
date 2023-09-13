669029a9c765dc06fa0f581e471d02ebbb062fd3
Erdem Alptuğ Öksüz <81254185+erdemalptugoksuz@users.noreply.github.com>
2023-09-13 16:18:07 +0300

    get and post request's added

--- app.js
+++ app.js
@@ -1,16 +1,48 @@
 import express from "express";
+import fs from "fs";
 
 const application = express();
+application.use(express.json());
 const port = 3000;
+const tours = JSON.parse(fs.readFileSync(`./dev-data/data/tours-simple.json`));
 
-application.get("/", (request, response) => {
-  response
-    .status(200)
-    .json({ message: "Hello from the server side!", app: "Natours" });
+// application.get("/", (response) => {
+//   response
+//     .status(200)
+//     .json({ message: "Hello from the server side!", app: "Natours" });
+// });
+
+// application.post("/", (response) => {
+//   response.send("You can post to this endpoint...");
+// });
+
+application.get("/api/v1/tours", (request, response) => {
+  response.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
-application.post("/", (request, response) => {
-  response.send("You can post to this endpoint...");
+application.post("/api/v1/tours", (request, response) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, request.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    "./dev-data/data/tours-simple.json",
+    JSON.stringify(tours),
+    (error) => {
+      response.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 application.listen(port, () => {
