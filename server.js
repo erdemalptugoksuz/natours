8470c7009553bd4ddb34d57949337d14d819e74c
Erdem Alptuğ Öksüz <81254185+erdemalptugoksuz@users.noreply.github.com>
2023-09-17 01:18:43 +0300

    Added mongoDB connection

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 const application = require("./app");
 
 dotenv.config({ path: "./config.env" });
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD,
+);
+mongoose.connect(DB).then(() => {
+  console.log("DB connection successful!");
+});
 
 const port = process.env.PORT || 3000;
 application.listen(port, () => {
